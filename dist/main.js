baa0d0b0b2dda073dfea8510195132d70fff0734
DylanDeFrank <defrankdylan@gmail.com>
2025-01-07 10:39:28 -0800

    Added svg calendar and plus button
    Need to figure out why the css isn't loading onto my webpage.

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 /*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
@@ -16,7 +16,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _task_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task.js */ \"./src/task.js\");\n/* harmony import */ var _list_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./list.js */ \"./src/list.js\");\n\n        \n    \n\n    let walk = new _task_js__WEBPACK_IMPORTED_MODULE_0__.Task('walk')\n    console.log(walk.name)\n\n    let shop = new _task_js__WEBPACK_IMPORTED_MODULE_0__.Task('shop')\n    console.log(shop.name)\n    \n    \n    let list = new _list_js__WEBPACK_IMPORTED_MODULE_1__.List('list')\n    list.addTask(walk)\n    list.addTask(shop)\n    console.log(list.showList())\n\n    let list2 = new _list_js__WEBPACK_IMPORTED_MODULE_1__.List('list2')\n    list2.addTask(shop)\n    console.log(list2.showList())\n\n\n//# sourceURL=webpack://todo/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _task_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task.js */ \"./src/task.js\");\n/* harmony import */ var _list_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./list.js */ \"./src/list.js\");\n\n        \n    \n\n    let walk = new _task_js__WEBPACK_IMPORTED_MODULE_0__.Task('walk')\n    console.log(walk.name)\n\n    let shop = new _task_js__WEBPACK_IMPORTED_MODULE_0__.Task('shop')\n    console.log(shop.name)\n    \n    \n    let list = new _list_js__WEBPACK_IMPORTED_MODULE_1__.List('list')\n    list.addTask(walk)\n    list.addTask(shop)\n    console.log(list.showList())\n\n    let list2 = new _list_js__WEBPACK_IMPORTED_MODULE_1__.List('list2')\n    list2.addTask(shop)\n    console.log(list2.showList())\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7O0FBQWdDO0FBQ0E7QUFDaEM7O0FBRUEsbUJBQW1CLDBDQUFJO0FBQ3ZCOztBQUVBLG1CQUFtQiwwQ0FBSTtBQUN2QjtBQUNBO0FBQ0E7QUFDQSxtQkFBbUIsMENBQUk7QUFDdkI7QUFDQTtBQUNBOztBQUVBLG9CQUFvQiwwQ0FBSTtBQUN4QjtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby8uL3NyYy9pbmRleC5qcz9iNjM1Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFRhc2sgfSBmcm9tIFwiLi90YXNrLmpzXCJcbmltcG9ydCB7IExpc3QgfSBmcm9tIFwiLi9saXN0LmpzXCIgICAgICAgIFxuICAgIFxuXG4gICAgbGV0IHdhbGsgPSBuZXcgVGFzaygnd2FsaycpXG4gICAgY29uc29sZS5sb2cod2Fsay5uYW1lKVxuXG4gICAgbGV0IHNob3AgPSBuZXcgVGFzaygnc2hvcCcpXG4gICAgY29uc29sZS5sb2coc2hvcC5uYW1lKVxuICAgIFxuICAgIFxuICAgIGxldCBsaXN0ID0gbmV3IExpc3QoJ2xpc3QnKVxuICAgIGxpc3QuYWRkVGFzayh3YWxrKVxuICAgIGxpc3QuYWRkVGFzayhzaG9wKVxuICAgIGNvbnNvbGUubG9nKGxpc3Quc2hvd0xpc3QoKSlcblxuICAgIGxldCBsaXN0MiA9IG5ldyBMaXN0KCdsaXN0MicpXG4gICAgbGlzdDIuYWRkVGFzayhzaG9wKVxuICAgIGNvbnNvbGUubG9nKGxpc3QyLnNob3dMaXN0KCkpXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
 
 /***/ }),
 
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _tas
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   List: () => (/* binding */ List)\n/* harmony export */ });\nclass List {\n    constructor() {\n        this.list = []\n    }\n    addTask(task) {\n        this.list.push(task)\n    }\n    showList() {\n        return (this.list)\n    }\n}\n\n//# sourceURL=webpack://todo/./src/list.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   List: () => (/* binding */ List)\n/* harmony export */ });\nclass List {\n    constructor() {\n        this.list = []\n    }\n    addTask(task) {\n        this.list.push(task)\n    }\n    showList() {\n        return (this.list)\n    }\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbGlzdC5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQU87QUFDUDtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8vLi9zcmMvbGlzdC5qcz80YmVjIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjbGFzcyBMaXN0IHtcbiAgICBjb25zdHJ1Y3RvcigpIHtcbiAgICAgICAgdGhpcy5saXN0ID0gW11cbiAgICB9XG4gICAgYWRkVGFzayh0YXNrKSB7XG4gICAgICAgIHRoaXMubGlzdC5wdXNoKHRhc2spXG4gICAgfVxuICAgIHNob3dMaXN0KCkge1xuICAgICAgICByZXR1cm4gKHRoaXMubGlzdClcbiAgICB9XG59Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/list.js\n");
 
 /***/ }),
 
@@ -36,7 +36,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Task: () => (/* binding */ Task)\n/* harmony export */ });\nclass Task {\n    constructor(name, description, dueDate, priority) {\n        this.name = name\n        this.description = description\n        this.dueDate = dueDate\n        this.priority = priority\n    }\n}\n\n\n//# sourceURL=webpack://todo/./src/task.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Task: () => (/* binding */ Task)\n/* harmony export */ });\nclass Task {\n    constructor(name, description, dueDate, priority) {\n        this.name = name\n        this.description = description\n        this.dueDate = dueDate\n        this.priority = priority\n    }\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdGFzay5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQU87QUFDUDtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8vLi9zcmMvdGFzay5qcz83YzExIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjbGFzcyBUYXNrIHtcbiAgICBjb25zdHJ1Y3RvcihuYW1lLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHkpIHtcbiAgICAgICAgdGhpcy5uYW1lID0gbmFtZVxuICAgICAgICB0aGlzLmRlc2NyaXB0aW9uID0gZGVzY3JpcHRpb25cbiAgICAgICAgdGhpcy5kdWVEYXRlID0gZHVlRGF0ZVxuICAgICAgICB0aGlzLnByaW9yaXR5ID0gcHJpb3JpdHlcbiAgICB9XG59XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/task.js\n");
 
 /***/ })
 
@@ -99,7 +99,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
 /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,20 @@ module.exports = {
         template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource", 
+        },
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+    ],
+  },
 };
