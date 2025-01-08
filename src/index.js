ebf28dee12a65d55f201d0d75035f1c56c19d1ab
DylanDeFrank <defrankdylan@gmail.com>
2025-01-08 10:07:02 -0800

    Created button in that adds a task container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import { Task } from "./task.js"
-import { List } from "./list.js"        
+import { List } from "./list.js"   
+import "./styles.css"     
+
+const button = document.querySelector('.addTask')
+
+button.addEventListener ('click', function (e) {
+    createTask()
+
     
+})
+function createTask () {
+    const taskContainer = document.createElement("div")
+    taskContainer.classList.add("taskContainer")
+    const header = document.createElement("div.header")
+    const title = document.createElement("h1.title")
+    const dueDate = document.createElement("div.dueDate")
+    const footer = document.createElement("div.footer")
+    const taskNotes = document.createElement("p.taskNotes")
+    const priority = document.createElement("div.priority")
+    const body = document.querySelector("body")
+
+    body.appendChild(taskContainer)
+}
 
     let walk = new Task('walk')
     console.log(walk.name)
